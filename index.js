ed49920f745ba6c557a55ec1ba101dac9bb5195a
Aaron Sala <salaaron2@gmail.com>
2024-03-14 00:27:01 +0300

    stop watch

---
+++ index.js
@@ -0,0 +1,21 @@
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+const reset = document.querySelector(".reset");
+
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const milisecond = document.querySelector(".milisecond");
+
+function updateCounter() {
+  // Increment the counter value to 1000 over 1 second
+ 
+}
+let counter = 0;
+start.addEventListener("click", () => {
+ 
+});
+stop.addEventListener("click", () => {
+  clearInterval()
+  console.log("counter stoped");
+});
